63260e330f55f1fd31fc2409c65544ebcd489071
Lucas <lucasmacedo9@hotmail.com>
2022-09-01 19:51:52 -0300

    class banco

--- main.py
+++ main.py
@@ -3,6 +3,26 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from CadAluno import Ui_CadAlunos
 import pymysql.cursors
 
+class Banco:
+    def __init__(self):
+        self.conexao = pymysql.connect(
+            host="localhost",
+            user="root",
+            password=,
+            database="escola",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+
+    def inserirAluno(self,nome,idade,curso):
+        with self.conexao.cursor() as curso:
+            try:
+                sql = "INSERT INTO aluno (nome, idade, curso) VALUES (%s, %s, %s)"
+                curso.execute(sql(nome, idade, curso))
+                self.conexao.commit()
+            except Exception as erro:
+                print(f'erro ao cadastrar: {erro}')
+
+
 
 
 class Janela(QMainWindow, Ui_CadAlunos):
